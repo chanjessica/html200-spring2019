83ec5ebb9aece3263657c549ead5996d7b6d33f7
chanjessica <43660859+chanjessica@users.noreply.github.com>
2019-04-17 08:22:52 -0700

    loop to all of the elements

---
+++ Lesson03/excercise-three.js
@@ -0,0 +1,10 @@
+/*
+   Create an array has at least 5 strings:
+	Use a for loop to console log all of the elements plus the index and a comma
+*/
+
+let myArray=['Lesson','03','Assighment','3'];
+
+for(i=0; i<myArray.length; i++){
+   console.log(i + ',' + myArray[i]) ;
+}
