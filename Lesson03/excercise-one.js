297b31f5dcd8d684e9e026d9284c6314634b38f7
chanjessica <43660859+chanjessica@users.noreply.github.com>
2019-04-17 08:17:14 -0700

    Create excercise-one.js

---
+++ Lesson03/excercise-one.js
@@ -0,0 +1,11 @@
+let myArray=['JavaScript','arrays','are','used','to','store','multiple','values','in','a','single','variable'];
+console.log(myArray);
+
+myArray.push(".");
+console.log(myArray);
+
+myArray.splice(2,1);
+console.log(myArray);
+
+intoStr=myArray.join(',');
+console.log(intoStr);
