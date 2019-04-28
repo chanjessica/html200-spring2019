0298db634de456a761819af951963364d130330e
chanjessica <43660859+chanjessica@users.noreply.github.com>
2019-04-27 18:36:39 -0700

    Update hw4-1.js
    add space between =

--- Lesson04/hw4-1.js
+++ Lesson04/hw4-1.js
@@ -1,30 +1,30 @@
-function greeting()
-{
-   alert('Welcome to the World!')
+function greeting(){
+   alert('Welcome to the World!');
 }
 
 function sqIt(){
-   let x=prompt('please enter a numer:');
-   alert(x+'*'+x+'='+Number(x)*Number(x));
+   let x = prompt('please enter a numer:');
+   alert(x + '*' + x + '=' + Number(x) * Number(x));
 }
+
 function mathIt(){
-   let a=prompt('please enter your numer:');
-   let operator=prompt('enter the math operator (+ - * / %): ');
-   let b=prompt('please enter your other numer:');
-   let result=0;
+   let a = prompt('please enter your numer:');
+   let operator = prompt('enter the math operator (+ - * / %): ');
+   let b = prompt('please enter your other numer:');
+   let result = 0;
 
    if (operator == '+' || operator =='add' || operator == 'ADD') {
-      result= Number(a)+Number(b);
+      result = Number(a) + Number(b);
    } else if (operator == '-' || operator == 'subtract' || operator == 'SUBTRACT') {
-      result= Number(a)-Number(b);
+      result = Number(a) - Number(b);
    } else if (operator == '*' || operator =='multiply' || operator == 'MULTIPLY') {
-      result= Number(a)*Number(b);
+      result = Number(a) * Number(b);
    } else if (operator == '/'  || operator =='divide'   || operator =='DIVIDE' ){
-      result= Number(a)/Number(b);
+      result = Number(a) / Number(b);
    } else if (operator == '%'  || operator == 'mod' || operator == 'MOD' ){
-      result= Number(a)%Number(b);
+      result = Number(a) % Number(b);
    }
-   log=a + ' ' + operator +' '+ b + ' = ' + result ;
+   log = a + ' ' + operator + ' ' + b + ' = ' + result ;
    console.log(' result is ' + log); 
    alert(log);
 }
